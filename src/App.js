a2080cd72bb6bed379e7ec173f5be00766cde78f
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2021-02-16 20:34:01 +0200

    nav refactoring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import './index.scss';
 import Navigation from './components/Navigation/Navigation';
 import { Component } from 'react';
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
       currentDate: new Date(),
     };
   }
+
+  prevMonth = () => {
+    this.setState({
+      currentDate: new Date(
+        this.state.currentDate.getFullYear(),
+        this.state.currentDate.getMonth() - 1
+      ),
+    });
+  };
+
+  nextMonth = () => {
+    this.setState({
+      currentDate: new Date(
+        this.state.currentDate.getFullYear(),
+        this.state.currentDate.getMonth() + 1
+      ),
+    });
+  };
+
   render() {
-    return <Navigation date={this.state.currentDate} />;
+    return (
+      <div className="calendar">
+        <Navigation
+          date={this.state.currentDate}
+          prevMonth={this.prevMonth}
+          nextMonth={this.nextMonth}
+        />
+      </div>
+    );
   }
 }
-
-export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,49 +6,25 @@ const NavButton = ({ modifier, ClickAction }) => {
       className={
         'calendar-bar__control-button calendar-bar__control-button--' + modifier
       }
-      onClick={ClickAction}
+      onClick={() => {
+        ClickAction();
+      }}
     ></button>
   );
 };
 
 export default class Navigation extends Component {
-  constructor({ date }) {
-    super();
-
-    this.state = {
-      currentDate: date,
-    };
-  }
-
-  prevMonth = () => {
-    this.setState({
-      currentDate: new Date(
-        this.state.currentDate.getFullYear(),
-        this.state.currentDate.getMonth() - 1
-      ),
-    });
-  };
-
-  nextMonth = () => {
-    this.setState({
-      currentDate: new Date(
-        this.state.currentDate.getFullYear(),
-        this.state.currentDate.getMonth() + 1
-      ),
-    });
-  };
-
   render() {
     return (
       <div className="calendar-bar">
-        <NavButton modifier="prev" ClickAction={this.prevMonth} />
+        <NavButton modifier="prev" ClickAction={this.props.prevMonth} />
         <h2 className="calendar-bar__header">
-          {this.state.currentDate.toLocaleDateString('en-US', {
+          {this.props.date.toLocaleDateString('en-US', {
             month: 'long',
             year: 'numeric',
           })}
         </h2>
-        <NavButton modifier="next" ClickAction={this.nextMonth} />
+        <NavButton modifier="next" ClickAction={this.props.nextMonth} />
       </div>
     );
   }
